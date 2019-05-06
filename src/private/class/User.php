f8acdd3ae33d66ecbd19c4542e1c595d72f3d94c
GeestWagen <nkolpa12@gmail.com>
2019-05-06 23:57:39 +0200

    added the review datamodel

---
+++ src/private/class/User.php
@@ -0,0 +1,9 @@
+<?php
+
+class User
+{
+    public function getName()
+    {
+        return "Not implemented";
+    }
+}
--- src/private/controller/HomeController.php
+++ src/private/controller/HomeController.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once config["CLASS_FOLDER"] . "Review.php";
 class HomeController extends Controller
 {
     public function createView()
     {
-        $this->loadView("Home.phtml", array("title" => "Home"));
+        $data = array("title" => "Home");
+        $data["reviewArray"] = array(new Review(10, new User(), "Mijn review", "helemaal mooi", new DateTime(), 7.4, 0));
+
+        $this->loadView("Home.phtml", $data);
     }
 }
