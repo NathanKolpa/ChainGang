531f2d032d0fa5a0f7eef93c1438588100427d84
GeestWagen <nkolpa12@gmail.com>
2019-05-06 23:20:19 +0200

    added the upvote count to change as you vote

--- src/public/js/Review.js
+++ src/public/js/Review.js
@@ -1,6 +1,31 @@
+function vote(commentID, isUp)
+{
+
+}
+
+function removeVote(commentID)
+{
+
+}
+
+function changeVoteText(commentID, delta)
+{
+    var element = document.getElementById("count_" + commentID);
+    var text = "";
+
+    if(parseInt(element.innerText) + delta > 0)
+    {
+        //text += "+"; weet niet zeker of dit nodig is
+    }
+
+    text += parseInt(element.innerText) + delta;
+    element.innerText = text;
+}
+
 
 function voteButton(button)
 {
+
     var commentID = button.id.split("_")[1];
     var type =  button.id.split("_")[0];
 
@@ -8,7 +33,16 @@ function voteButton(button)
     if(button.classList.contains("VoteSelected"))
     {
         button.classList.remove("VoteSelected");
+        if(type == "up")
+        {
+            changeVoteText(commentID, -1);
+        }
+        else
+        {
+            changeVoteText(commentID, 1);
+        }
         //remove vote
+        removeVote(parseInt(commentID));
     }
     else
     {
@@ -18,17 +52,28 @@ function voteButton(button)
         if(type == "up")
         {
             oposingButton = document.getElementById("down_" + commentID);
+            changeVoteText(commentID, 1);
         }
         else
         {
             oposingButton = document.getElementById("up_" + commentID);
+            changeVoteText(commentID, -1);
         }
 
         if(oposingButton.classList.contains("VoteSelected"))
         {
             oposingButton.classList.remove("VoteSelected");
+
+            if(type == "up")
+            {
+                changeVoteText(commentID, 1);
+            }
+            else
+            {
+                changeVoteText(commentID, -1);
+            }
         }
         //add/change vote
+        vote(parseInt(commentID), type == "up" ? true : false);
     }
-
 }
