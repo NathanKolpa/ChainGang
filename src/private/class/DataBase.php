827ba0b64cf371dc29279bd1d37a265f2b9f6614
GeestWagen <nkolpa12@gmail.com>
2019-05-08 15:13:47 +0200

    changed database

--- src/private/class/DataBase.php
+++ src/private/class/DataBase.php
@@ -7,14 +7,16 @@ class DataBase
 
     public function __construct()
     {
-        include config["CONFIG_FOLDER"] . "dbconfig.php";
+        $serverName = "localhost";
+        $userName = "nkolpa_root";
+        $password = "pikhoofd123";
         
         $this->dbConnection = new mysqli($serverName, $userName, $password);
         $this->dbConnection->set_charset("utf8");
 
         if ($this->dbConnection->connect_error)
         {
-            die("Failed to connect to server: " . $serverName . " e:" . $connection->connect_error);
+            die("Failed to connect to server: " . $serverName . " e:" . $this->dbConnection->connect_error);
         }
         else
         {
