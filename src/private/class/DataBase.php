d7842b2a452d718dcbeb825c6874fb8efc2f8d70
GeestWagen <nkolpa12@gmail.com>
2019-05-08 21:19:30 +0200

    fixed the query issiue

--- src/private/class/DataBase.php
+++ src/private/class/DataBase.php
@@ -10,26 +10,28 @@ class DataBase
         $serverName = "localhost";
         $userName = "nkolpa_root";
         $password = "pikhoofd123";
-        $dbName = "chaingang";
+        $dbName = "nkolpa_chaingang";
         
-        $this->dbConnection = new mysqli($serverName, $userName, $password);
-        $this->dbConnection->set_charset("utf8");
+        $this->dbConnection = new mysqli($serverName, $userName, $password, $dbName);
+       // $this->dbConnection->set_charset("utf8");
 
         if ($this->dbConnection->connect_error)
         {
             die("Failed to connect to server: " . $serverName . " e:" . $this->dbConnection->connect_error);
         }
-        else
-        {
-            if (!mysqli_select_db($this->dbConnection,$dbName))
-            {
-                die("Failed to use to data base: " . $dbName);
-            }
-        }
     }
 
     public function querry(string $querry)
     {
-        return $this->dbConnection->multi_query($querry);
+        $result = $this->dbConnection->query($querry);
+        if($result != false)
+        {
+            return $result;
+
+        }
+        else
+        {
+            die($result->error);
+        }
     }
 }
