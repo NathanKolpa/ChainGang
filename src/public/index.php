012e91114e69bec940e11dedc0ec163ebcef37d6
GeestWagen <nkolpa12@gmail.com>
2019-05-08 13:35:22 +0200

    fixed the get

--- src/public/index.php
+++ src/public/index.php
@@ -8,4 +8,4 @@ error_reporting(E_ALL);
 define("PRIVATE_FOLDER", realpath(dirname(__FILE__) . "/../private"));
 require_once(PRIVATE_FOLDER . "/config/config.php");
 //echo $_GET["page"];
-Route::loadPage("");
+Route::loadPage($_GET["page"]);
