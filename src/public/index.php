190d77fb49662503bf83bb69d3d8a8eba6d89cbe
GeestWagen <nkolpa12@gmail.com>
2019-05-08 13:29:31 +0200

    added error reporting

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,11 @@
 <?php
 
 session_start();
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 
 //define("PRIVATE_FOLDER", "../private");
 define("PRIVATE_FOLDER", realpath(dirname(__FILE__) . "/../private"));
 require_once(PRIVATE_FOLDER . "/config/config.php");
 //echo $_GET["page"];
-Route::loadPage($_GET["page"]);
+Route::loadPage("");
