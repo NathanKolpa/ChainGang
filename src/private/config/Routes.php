b585d266d3d98a6cbfa7b2f25a71c01aa492307f
Jesse <jesse.helmes@student.graafschapcollege.nl>
2019-05-15 17:39:48 +0200

    reposive producten list
    de catogorie pagina heeft een reposive fietsen lijst template

--- src/private/config/Routes.php
+++ src/private/config/Routes.php
@@ -3,9 +3,11 @@ require_once config["CONTROLLER_FOLDER"] . "HomeController.php";
 require_once config["CONTROLLER_FOLDER"] . "NotFoundController.php";
 require_once config["CONTROLLER_FOLDER"] . "LoginController.php";
 require_once config["CONTROLLER_FOLDER"] . "RegisterController.php";
+require_once config["CONTROLLER_FOLDER"] . "CatogorieController.php";
 
 Route::addRoute("", new HomeController($dataBase));
 Route::addRoute("home", new HomeController($dataBase));
 Route::addRoute("404", new NotFoundController($dataBase));
 Route::addRoute("login", new LoginController($dataBase));
 Route::addRoute("register", new RegisterController($dataBase));
+Route::addRoute("catogorie", new CatogorieController($dataBase));
