ddea2ce606d652a6847a0671b1bf7270efb67a1c
michiellegters123 <michiel@legters.org>
2019-05-15 14:37:41 +0200

    added shit

--- src/private/config/Routes.php
+++ src/private/config/Routes.php
@@ -5,6 +5,7 @@ require_once config["CONTROLLER_FOLDER"] . "LoginController.php";
 require_once config["CONTROLLER_FOLDER"] . "RegisterController.php";
 require_once config["CONTROLLER_FOLDER"] . "ProfielController.php";
 require_once config["CONTROLLER_FOLDER"] . "BestellingController.php";
+require_once config["CONTROLLER_FOLDER"] . "FacturenController.php";
 
 Route::addRoute("", new HomeController($dataBase));
 Route::addRoute("home", new HomeController($dataBase));
@@ -12,3 +13,4 @@ Route::addRoute("404", new NotFoundController($dataBase));
 Route::addRoute("login", new LoginController($dataBase));
 Route::addRoute("profiel", new ProfielController($dataBase));
 Route::addRoute("profiel/bestellingen", new BestellingController($dataBase));
+Route::addRoute("profiel/facturen", new FacturenController($dataBase));
